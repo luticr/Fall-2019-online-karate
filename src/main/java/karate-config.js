a3c951019b82143ec93e4faff3ed37bcef72e786
cristi <luticr@gmail.com>
2020-06-02 20:22:48 -0400

    karate config file

--- src/main/java/karate-config.js
+++ src/main/java/karate-config.js
@@ -1,23 +1,18 @@
 function fn() {
-  var env = karate.env; // get java system property 'karate.env'
+  var env = karate.env; // get system property 'karate.env'
   karate.log('karate.env system property was:', env);
   if (!env) {
-    env = 'dev'; // a custom 'intelligent' default
+    env = 'dev';
   }
-  var config = { // base config JSON
-    appId: 'my.app.id',
-    appSecret: 'my.secret',
-    someUrlBase: 'https://some-host.com/v1/auth/',
-    anotherUrlBase: 'https://another-host.com/v1/'
-  };
-  if (env == 'stage') {
-    // over-ride only those that need to be
-    config.someUrlBase = 'https://stage-host/v1/auth';
+  var config = {
+    env: env,
+	myVarName: 'someValue'
+  }
+  if (env == 'dev') {
+    // customize
+    // e.g. config.foo = 'bar';
   } else if (env == 'e2e') {
-    config.someUrlBase = 'https://e2e-host/v1/auth';
+    // customize
   }
-  // don't waste time waiting for a connection or if servers don't respond within 5 seconds
-  karate.configure('connectTimeout', 5000);
-  karate.configure('readTimeout', 5000);
   return config;
 }
